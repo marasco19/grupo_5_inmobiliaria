cd3f5ff199bd2f55ef3f469383701b636947d7b6
marasco19 <marasco19@gmail.com>
2021-02-03 09:40:48 -0300

    agrego popup de login

--- app.js
+++ app.js
@@ -18,3 +18,7 @@ app.get('/productDetail', (req,res) => {
     res.sendFile(path.resolve('./views/productDetail.html'));
 });
 
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve('./views/login.html'));
+});
+
